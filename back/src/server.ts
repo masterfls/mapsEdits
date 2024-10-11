77c47ba1b9be2b719f1c4dc7db4f70a32b5ab36c
masterfls <fernandeztomas735@gmail.com>
2024-10-11 17:55:13 -0300

    cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -9,10 +9,13 @@ const app = express();          //creo mi servidor en la constante app
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-    origin: 'https://ievg.online', // Permitir solicitudes desde el dominio de tu frontend
+    origin: process.env.NODE_ENV === 'production' 
+        ? 'https://ievg-online-21570d3c5e46.herokuapp.com' // Cambia esto a la URL de tu app en Heroku
+        : 'http://localhost:3000', // Cambia esto a la URL de tu frontend local
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(router)         //tengo el servidor, el enrutador, se hace la peticion y en esta linea lego hacia donde ira la solicitud, en este caso al anrutador
 
